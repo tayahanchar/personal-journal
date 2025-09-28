2547931a4e42dfc007488eed9ced42d163127583
Taya Hanchar <tayahanchar@gmail.com>
2025-09-28 16:19:22 +0200

    feat: add NodesSection

---
+++ src/Components/NodesSection/NodesSection.jsx
@@ -0,0 +1,35 @@
+import './NodesSection.css';
+
+const NodesSection = () => {
+  return (
+    <section className='notes-section'>
+      <h1 className='notes-section_title'>Personal Journal</h1>
+      <button className='new-note-button'>+ New note</button>
+      <div className='notes-list'>
+        <div className='notes-section_note'>
+          <p className='note-title'>note 1</p>
+          <div className='note-description'>
+            <span className='note-date'>30.05.2025</span>
+            <p className='note-text'>djvldjnl ffff ddgd.    gb.   db  ksnjdlj.   ddd vnsdddd ldk alksjnl.   e.  eee ssdvsdf sdvdfdgs dfgdrgdr ersfrfgrd dvdrfvdrg dfgdrgdrg ss cskd fff jn skjndlcn</p>
+          </div>
+        </div>
+        <div className='notes-section_note'>
+          <p className='note-title'>note 1</p>
+          <div className='note-description'>
+            <span className='note-date'>30.05.2025</span>
+            <p className='note-text'>djvldjnl ffff ddgd.    gb.   db  ksnjdlj.   ddd vnsdddd ldk alksjnl.   e.  eee ssdvsdf sdvdfdgs dfgdrgdr ersfrfgrd dvdrfvdrg dfgdrgdrg ss cskd fff jn skjndlcn</p>
+          </div>
+        </div>
+        <div className='notes-section_note'>
+          <p className='note-title'>note 1</p>
+          <div className='note-description'>
+            <span className='note-date'>30.05.2025</span>
+            <p className='note-text'>djvldjnl ffff ddgd.    gb.   db  ksnjdlj.   ddd vnsdddd ldk alksjnl.   e.  eee ssdvsdf sdvdfdgs dfgdrgdr ersfrfgrd dvdrfvdrg dfgdrgdrg ss cskd fff jn skjndlcn</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default NodesSection;
