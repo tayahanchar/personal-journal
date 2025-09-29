6eb614eeccb90670fb36afe36d3a88c8e62cbc27
Taya Hanchar <tayahanchar@gmail.com>
2025-09-29 22:02:19 +0200

    feat: change some styles in notes components

--- src/Components/NotesSection/NotesSection.jsx
+++ src/Components/NotesSection/NotesSection.jsx
@@ -9,21 +9,21 @@ const NotesSection = () => {
         <div className='notes-section_note'>
           <p className='note-title'>note 1</p>
           <div className='note-description'>
-            <span className='note-date'>30.05.2025</span>
+            <span className='notes-date'>30.05.2025</span>
             <p className='note-text'>djvldjnl ffff ddgd.    gb.   db  ksnjdlj.   ddd vnsdddd ldk alksjnl.   e.  eee ssdvsdf sdvdfdgs dfgdrgdr ersfrfgrd dvdrfvdrg dfgdrgdrg ss cskd fff jn skjndlcn</p>
           </div>
         </div>
         <div className='notes-section_note'>
           <p className='note-title'>note 1</p>
           <div className='note-description'>
-            <span className='note-date'>30.05.2025</span>
+            <span className='notes-date'>30.05.2025</span>
             <p className='note-text'>djvldjnl ffff ddgd.    gb.   db  ksnjdlj.   ddd vnsdddd ldk alksjnl.   e.  eee ssdvsdf sdvdfdgs dfgdrgdr ersfrfgrd dvdrfvdrg dfgdrgdrg ss cskd fff jn skjndlcn</p>
           </div>
         </div>
         <div className='notes-section_note'>
           <p className='note-title'>note 1</p>
           <div className='note-description'>
-            <span className='note-date'>30.05.2025</span>
+            <span className='notes-date'>30.05.2025</span>
             <p className='note-text'>djvldjnl ffff ddgd.    gb.   db  ksnjdlj.   ddd vnsdddd ldk alksjnl.   e.  eee ssdvsdf sdvdfdgs dfgdrgdr ersfrfgrd dvdrfvdrg dfgdrgdrg ss cskd fff jn skjndlcn</p>
           </div>
         </div>
