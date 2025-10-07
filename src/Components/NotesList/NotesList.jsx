62b11568ae60fd0631990a5ee58583cd6e60b750
Taya Hanchar <tayahanchar@gmail.com>
2025-10-07 22:37:38 +0200

    feat: add list sort

--- src/Components/NotesList/NotesList.jsx
+++ src/Components/NotesList/NotesList.jsx
@@ -2,9 +2,13 @@ import "./NotesList.css";
 import { ListNote } from "../ListNote";
 
 export const NotesList = ({ notesList }) => {
+  const sortNotes = (a, b) => {
+    return Date.parse(b.date) - Date.parse(a.date);
+  };
+
   return (
     <div className="notes-list">
-      {notesList.map((note) => (
+      {notesList.sort(sortNotes).map((note) => (
         <ListNote key={note.id} note={note} />
       ))}
     </div>
